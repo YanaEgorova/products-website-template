9eeb1b663957e2da88dfb8d1c5532c5850733864
YanaEgorova <45537884+YanaEgorova@users.noreply.github.com>
2023-08-02 00:32:33 +0300

    Update product-item.js

--- js/templates/product-item.js
+++ js/templates/product-item.js
@@ -2,6 +2,7 @@ export const item = product => {
   return `
   <li class="item">
   <div class="product__block js_product glass glass-m" id="${product.id}">
+     <div class="product__block--inner">
     <div class="product__img-box">
       <img src="${product.image}" alt="" class="img" />
     </div>
@@ -10,12 +11,13 @@ export const item = product => {
       <p class="product__name js_product-name">${product.name}</p>
       ${
         product.description[0].text.length > 0
-          ? '<p class="text product__desc">' + product.description[0].text.slice(0, 100) + '...' + '</p>'
+          ? '<p class="text product__desc">' + product.description[0].text.slice(0, 80) + '...' + '</p>'
           : ''
       }
       <span class="product__price">
         $${product.price.toFixed(2)}
       </span>
+    </div>
     </div>
     <div class="product__btn-block">
       <a href="product-page.html?id=${product.id}" class="btn btn--first" style="margin: ${
